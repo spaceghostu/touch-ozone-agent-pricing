4e0fa5b7eaaf914f1f7c3f3dd065f029b49461c7
Calvin Durrant <Calvin.Durrant@standardbank.co.za>
2021-10-18 17:43:37 +0200

    change field name

--- app/Components/FormSections/CustomerSearchSection.js
+++ app/Components/FormSections/CustomerSearchSection.js
@@ -8,7 +8,7 @@ export default function CustomerSearchSection() {
     return (
         <FormSection title="TOUCH OZONE (PTY) LTD CUSTOMER REGISTRATION AND SALES ORDER PROCESS" titleCenter>
             <CheckInput label="Existing Customer" width={30} name="existingCustomer" />
-            <TextInput label="Search" width={70} name="customer" />
+            <TextInput label="Customer" width={70} name="customer" />
             <TextInput label="Authorized Agent" width={50} name="authorizingAgent" />
             <TextInput label="Cell Number" width={50} name="cellNo" />
         </FormSection>
